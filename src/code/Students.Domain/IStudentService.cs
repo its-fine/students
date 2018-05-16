956f9639e12d00c93c5141a71a5414855345932f
David Palumbo <david.palumbo@hotmail.com>
2018-05-15 22:05:43 -0400

    Add initial domain structure.

---
+++ src/code/Students.Domain/IStudentService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace Students.Domain
+{
+    /// <summary>
+    /// Interface for types that will provide student service operations.
+    /// </summary>
+    public interface IStudentService
+    {
+        /// <summary>
+        /// Adds a new student to the repository.
+        /// </summary>
+        /// <param name="student">
+        /// Required instance of the <see cref="Student"/> to save.
+        /// </param>
+        /// <returns>
+        /// The unique identifier ot the student that was saved.
+        /// </returns>
+        Task<int> AddStudent(Student student);
+    }
+}
