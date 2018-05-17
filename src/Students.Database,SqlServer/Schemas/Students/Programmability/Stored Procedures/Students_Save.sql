bd6847a8922bf08102d3732b4e8193bb7d53df05
David Palumbo <david.palumbo@hotmail.com>
2018-05-17 16:05:19 -0400

    Add database project.

---
+++ src/Students.Database,SqlServer/Schemas/Students/Programmability/Stored Procedures/Students_Save.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [Students].[Students_Save]
+(
+    @Id int,
+    @FirstName nvarchar(100),
+    @LastName nvarchar(100),
+    @ReturnId int output
+)
+AS
+
+    INSERT INTO
+        [Students].[Students]
+        (
+            [FirstName],
+            [LastName]
+        )
+        VALUES
+        (
+            @FirstName,
+            @LastName
+        )
+
+    SET @ReturnId = SCOPE_IDENTITY()
