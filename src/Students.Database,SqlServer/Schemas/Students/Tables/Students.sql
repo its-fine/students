f70dcc6a8d3e98cef1df84660c2d75557ec57ca7
David Palumbo <david-palumbo@users.noreply.github.com>
2018-05-17 22:50:11 -0400

    Add database project. (#3)
    * Add database project.
    
    * Update appveyoer tobuild solution directly.
    
    * Update appveyor configuration to use msbuild.
    
    * Add dotnet resotre to build.
    
    * Use nuget instead of dotnet.

---
+++ src/Students.Database,SqlServer/Schemas/Students/Tables/Students.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [Students].[Students]
+(
+    [Id] INT IDENTITY(1,1) NOT NULL,
+    [FirstName] NVARCHAR(100) NOT NULL,
+    [LastName] NVARCHAR(100) NOT NULL, 
+    CONSTRAINT [PK_Students] PRIMARY KEY ([Id])
+)
